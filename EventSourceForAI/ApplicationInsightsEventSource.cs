5673712cd9d74553c8b88178cd9fc184799dca1f
Sergey Kanzhelev <SergKanz@microsoft.com>
2015-09-28 09:12:47 -0700

    first version of EventSource for RequestTelemetry

---
+++ EventSourceForAI/ApplicationInsightsEventSource.cs
@@ -0,0 +1,15 @@
+﻿namespace EventSourceForAI
+{
+    using System.Diagnostics.Tracing;
+
+    internal class ApplicationInsightsEventSource : EventSource
+    {
+        public static ApplicationInsightsEventSource Log = new ApplicationInsightsEventSource();
+
+        [Event(1, Level = EventLevel.Informational)]
+        public void RequestSent(string instrumentationKey, string name, string id)
+        {
+            this.WriteEvent(1, instrumentationKey, name, id);
+        }
+    }
+}
---
+++ EventSourceForAI/EventSourceTelemetryInitializer.cs
@@ -0,0 +1,18 @@
+﻿namespace EventSourceForAI
+{
+    using Microsoft.ApplicationInsights.Extensibility;
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.DataContracts;
+
+    public class EventSourceTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry is RequestTelemetry)
+            {
+                var request = telemetry as RequestTelemetry;
+                ApplicationInsightsEventSource.Log.RequestSent(request.Context.InstrumentationKey, request.Name, request.Id);
+            }
+        }
+    }
+}
