4960aa85abf11c5359b14673ed636c9a644baeda
Sergey Kanzhelev <sergkanz@microsoft.com>
2015-10-09 08:10:12 -0700

    add rich payload event source

--- EventSourceForAI/EventSourceTelemetryInitializer.cs
+++ EventSourceForAI/EventSourceTelemetryInitializer.cs
@@ -1,18 +1,68 @@
 ﻿namespace EventSourceForAI
 {
-    using Microsoft.ApplicationInsights.Extensibility;
     using Microsoft.ApplicationInsights.Channel;
-    using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation.External;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 
     public class EventSourceTelemetryInitializer : ITelemetryInitializer
     {
+        private readonly EventSourceOptions eventSourceOptions;
+        private readonly EventSource eventSource;
+
+        public EventSourceTelemetryInitializer()
+        {
+            this.eventSource = new EventSource(
+                "Microsoft.ApplicationInsights",
+                EventSourceSettings.EtwSelfDescribingEventFormat,
+                "ETW_GROUP",
+                "{0d943590-b235-5bdb-f854-89520f32fc0b}");
+
+            this.eventSourceOptions = new EventSourceOptions() { Keywords = (EventKeywords)0x2000000000000 };
+
+        }
+
         public void Initialize(ITelemetry telemetry)
         {
             if (telemetry is RequestTelemetry)
             {
                 var request = telemetry as RequestTelemetry;
+
+                var requestData = new RequestData();
+                requestData.duration = request.Duration.ToString();
+                requestData.httpMethod = request.HttpMethod;
+                requestData.id = request.Id;
+                requestData.name = request.Name;
+                requestData.responseCode = request.ResponseCode;
+                requestData.startTime = request.StartTime.ToString();
+                requestData.success = request.Success;
+                requestData.url = request.Url == null ? "" : request.Url.ToString();
+                requestData.properties = request.Properties;
+
+                var tags = new Dictionary<string, string>();
+
+                var userCtx = new UserContextData(tags);
+                userCtx.AccountId = request.Context.User.AccountId;
+                userCtx.AcquisitionDate = request.Context.User.AcquisitionDate;
+                userCtx.Id = request.Context.User.Id;
+                userCtx.UserAgent = request.Context.User.UserAgent;
+
+                var operationCtx = new OperationContextData(tags);
+                operationCtx.Id = request.Context.Operation.Id;
+                operationCtx.Name = request.Context.Operation.Name;
+
+                this.WriteEvent("Request", request.Context.InstrumentationKey, tags, requestData);
+
                 ApplicationInsightsEventSource.Log.RequestSent(request.Context.InstrumentationKey, request.Name, request.Id);
             }
         }
+
+        internal void WriteEvent<T>(string eventName, string instrumentationKey, IDictionary<string, string> tags, T data)
+        {
+            this.eventSource.Write(eventName, this.eventSourceOptions, new { PartA_iKey = instrumentationKey, PartA_Tags = tags, _B = data });
+        }
+
     }
 }
---
+++ TestEventSource/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestEventSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = TelemetryConfiguration.CreateDefault();
+            config.TelemetryInitializers.Add(new EventSourceForAI.EventSourceTelemetryInitializer());
+            config.InstrumentationKey = "Foo";
+
+            TelemetryClient client = new TelemetryClient(config);
+
+            var request = new RequestTelemetry("TestRequest", DateTimeOffset.Now, TimeSpan.FromMilliseconds(10), "200", true);
+            request.Context.Properties.Add("property1", "value1");
+            request.Context.User.Id = "userId";
+
+            request.Context.Operation.Id = Guid.NewGuid().ToString();
+
+            client.TrackRequest(request);
+
+            Thread.Sleep(100);
+        }
+    }
+}
